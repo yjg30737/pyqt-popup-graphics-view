0327196e6490517e85eaaf31bdea1fe6fd677635
Jung Gyu Yoon <yjg30737@gmail.com>
2022-05-13 18:05:30 +0900

    Update setup.py to upload this to pypi

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
 from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
 
 setup(
     name='pyqt-popup-graphics-view',
@@ -7,8 +14,10 @@ setup(
     author_email='yjg30737@gmail.com',
     license='GPL 3.0',
     packages=find_packages(),
-    description='PyQt Popup QGraphicsView (Image will pop up when move the mouse cursor inside the QGraphicsView)',
+    description='PyQt popup QGraphicsView (Image will pop up when move the mouse cursor inside the QGraphicsView)',
     url='https://github.com/yjg30737/pyqt-popup-graphics-view.git',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
     install_requires=[
         'PyQt5>=5.8'
     ]
